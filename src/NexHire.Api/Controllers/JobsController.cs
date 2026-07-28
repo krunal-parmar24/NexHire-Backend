32ccb5bb5810179d2dbdc5a8db60d82df66f753b
krunal-parmar24 <krunalrparmar4@gmail.com>
2026-07-28 21:14:37 +0530

    Remove duplicate using directive in JobsController.cs
    Eliminated a redundant using statement for Microsoft.AspNetCore.Mvc to improve code clarity and maintainability.

--- src/NexHire.Api/Controllers/JobsController.cs
+++ src/NexHire.Api/Controllers/JobsController.cs
@@ -2,7 +2,6 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc;
 using NexHire.Application.DTOs.Jobs;
 using NexHire.Application.Interfaces;
 using NexHire.Application.Exceptions;
